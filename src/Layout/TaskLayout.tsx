d1b9a6aa60d892edc8b1e78145c9d378b0ff3372
RafatRU42 <rmrafat420@gmail.com>
2023-09-02 19:38:53 +0600

    task dashboard section added

---
+++ src/Layout/TaskLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskLayout = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TaskLayout
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,11 +3,13 @@ import MainLayout from '../Layout/MainLayout'
 import Home from '../Pages/Home'
 import Navbar from '../Pages/Navbar'
 import Task from '../Pages/Task'
+import TaskDashboard from '../TaskDashboard/TaskDashbord'
 
 export const router = createBrowserRouter([
     {path:'/', element:<MainLayout></MainLayout>,children:[
         {path:'/',element:<Home></Home>},
         {path:'/navbar',element:<Navbar/>},
         {path:'/task',element:<Task></Task>}
-    ]}
+    ]},
+    {path:'/taskDashboard', element:<TaskDashboard></TaskDashboard>}
 ])
